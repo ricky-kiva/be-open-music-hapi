0dac48090d6707af78651df86060ad7fddb1c4cc
ricky-kiva <ricky.kiva@gmail.com>
2024-09-14 16:58:50 +0700

    feat(validator): create `Songs` validator

---
+++ src/validator/songs/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const { SongPayloadSchema } = require('./schema');
+const InvariantError = require('../../exceptions/InvariantError');
+
+const SongsValidator = {
+  validatePayload: (payload) => {
+    const validationResult = SongPayloadSchema.validate(payload);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  }
+};
+
+module.exports = SongsValidator;
---
+++ src/validator/songs/schema.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const Joi = require('joi');
+
+const SongPayloadSchema = Joi.object({
+  title: Joi.string.max(255).required(),
+  year: Joi.number().required(),
+  genre: Joi.string().max(100).required(),
+  performer: Joi.string().max(255).required(),
+  duration: Joi.number(),
+  albumId: Joi.string.max(36)
+});
+
+module.exports = { SongPayloadSchema };
