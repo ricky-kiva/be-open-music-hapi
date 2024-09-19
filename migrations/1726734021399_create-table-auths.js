d6f2512abf094fc3db40273159162afe0fb97b24
ricky-kiva <ricky.kiva@gmail.com>
2024-09-19 15:32:12 +0700

    feat(db): add `auths` table migration

---
+++ migrations/1726734021399_create-table-auths.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('auths', {
+    token: {
+      type: 'TEXT',
+      notNull: true
+    }
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('auths');
+};
