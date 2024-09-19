018be5b7d620ada1149440211abbc72584c41530
ricky-kiva <ricky.kiva@gmail.com>
2024-09-19 23:11:44 +0700

    feat(service): add get all `playlists` service

--- src/services/postgres/PlaylistsService.js
+++ src/services/postgres/PlaylistsService.js
@@ -2,7 +2,9 @@
 
 const { Pool } = require('pg');
 const { nanoid } = require('nanoid');
+const { mapDBPlaylistToModel } = require('../../utils');
 const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
 
 class PlaylistsService {
   constructor() {
@@ -24,6 +26,31 @@ class PlaylistsService {
     }
 
     return result.rows[0].id;
+  }
+
+  async getAll(owner) {
+    const q = {
+      text: 'SELECT * FROM playlists WHERE owner = $1',
+      values: [owner]
+    };
+
+    const result = await this._pool.query(q);
+
+    return result.rows
+      .map(mapDBPlaylistToModel);
+  }
+
+  async deleteById(id) {
+    const q = {
+      text: 'DELETE FROM playlists WHERE id = $1 RETURNING id',
+      values: [id]
+    };
+
+    const result = await this._pool.query(q);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Playlist not found');
+    }
   }
 }
 
