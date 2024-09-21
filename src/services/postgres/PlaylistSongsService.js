ab64d4840ac2e56eca29c47843f2c5d397f1ac53
ricky-kiva <ricky.kiva@gmail.com>
2024-09-21 12:44:57 +0700

    feat(service): add get all `songs` in a `playlist` by `playlist id` method

--- src/services/postgres/PlaylistSongsService.js
+++ src/services/postgres/PlaylistSongsService.js
@@ -27,6 +27,21 @@ class PlaylistSongsService {
     }
   }
 
+  async getAll(playlistId) {
+    const q = {
+      text: `
+        SELECT songs.id, songs.title, songs.performer FROM songs
+        INNER JOIN playlist_songs ON playlist_songs.song_id = songs.id
+        WHERE playlist_songs.playlist_id = $1
+      `,
+      values: [playlistId]
+    };
+
+    const result = await this._pool.query(q);
+
+    return result.rows;
+  }
+
   async delete({ playlistId, songId }) {
     await this._songsService.getById(songId);
 
