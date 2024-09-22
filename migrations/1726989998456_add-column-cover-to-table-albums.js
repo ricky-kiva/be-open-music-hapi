7dcd51a99b1247931ddc7f8852fe3a13f6de9684
ricky-kiva <ricky.kiva@gmail.com>
2024-09-22 14:32:08 +0700

    feat(db): add `cover` col. to `albums` table migration

---
+++ migrations/1726989998456_add-column-cover-to-table-albums.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.addColumn('albums', {
+    cover: {
+      type: 'TEXT'
+    }
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropColumn('albums', 'owner');
+};
